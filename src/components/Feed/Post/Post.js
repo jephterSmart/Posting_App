71b3760a83734a2023347bd406541d778d84d50e
jephterSmart <kaskidsky@gmail.com>
2020-03-18 11:30:59 +0100

    push to heroku

---
+++ src/components/Feed/Post/Post.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import Button from '../../Button/Button';
+import './Post.css';
+
+const post = props =>{
+
+  let editDelete ;
+  if(props.isAuth) 
+  {editDelete = (
+    <div style={{display:'inline'}}>
+      <Button mode="flat" onClick={props.onStartEdit}>
+      Edit
+    </Button>
+    <Button mode="flat" design="danger" onClick={props.onDelete}>
+      Delete
+    </Button> 
+    </div>
+    
+) }
+else editDelete =  (
+  <div style={{display:'inline'}}>
+    <Button mode="flat" onClick={props.onStartEdit} disabled>
+    Edit
+  </Button>
+  <Button mode="flat"  onClick={props.onDelete} disabled>
+    Delete
+  </Button> 
+  </div>
+  
+);
+return (
+  <article className="post">
+    <header className="post__header">
+      <h3 className="post__meta">
+        Posted by {props.author} on {props.date}
+      </h3>
+      <h1 className="post__title">{props.title}</h1>
+    </header>
+    {/* <div className="post__image">
+      <Image imageUrl={props.image} contain />
+    </div>
+    <div className="post__content">{props.content}</div> */}
+    <div className="post__actions">
+      <Button mode="flat" link={props.id}>
+        View
+      </Button>
+      {editDelete}
+    </div>
+  </article>
+)
+};
+
+export default post;
